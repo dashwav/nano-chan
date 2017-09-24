d48cd0082e8de2d9858b6951fa6cf7c657e76c1a
dashwav <dashwav@gmail.com>
2017-09-24 13:39:35 -0700

    added reaction support

--- cogs/stats.py
+++ cogs/stats.py
@@ -20,6 +20,7 @@ class Stats:
     @checks.has_permissions(manage_emojis=True)
     async def stats_reactions(self, ctx):
         found_emojis = []
+        total_reactions = defaultdict(int)
         emoji_count = defaultdict(int)
         check_date = datetime.datetime.now() + datetime.timedelta(-30)
         for channel in ctx.message.guild.channels:
@@ -31,11 +32,15 @@ class Stats:
                 for word in message.content.split():
                     if '<:' in word:
                         found_emojis.append(word)
+                for reaction in message.reactions:
+                    total_reactions[reaction.emoji] += reaction.count
         print(found_emojis)
         for emoji_id in found_emojis:
             for emoji in ctx.message.guild.emojis:
                 if emoji_id == str(emoji):
                     emoji_count[emoji] += 1
+                    if emoji in total_reactions:
+                        emoji_count[emoji] += total_reactions[emoji]
         temp_str = 'Emoji use over last 30 days:\n'
         for key in sorted(emoji_count, key=emoji_count.get, reverse=True):
             temp_str += f'{key}: {emoji_count[key]}\n'
