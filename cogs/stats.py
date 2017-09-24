3365f0a7b63b0a86b178aca599aa645bfaf4aea6
dashwav <dashwav@gmail.com>
2017-09-24 13:12:48 -0700

    added send message with info

--- cogs/stats.py
+++ cogs/stats.py
@@ -4,6 +4,7 @@ Fun things with stats
 import re
 import discord
 import datetime
+from collections import defaultdict
 from .utils import helpers, checks
 from discord.ext import commands
 
@@ -19,6 +20,7 @@ class Stats:
     @checks.has_permissions(manage_emojis=True)
     async def stats_reactions(self, ctx):
         found_emojis = []
+        emoji_count = defaultdict(int)
         check_date = datetime.datetime.now() + datetime.timedelta(-30)
         for channel in ctx.message.guild.channels:
             try:
@@ -33,5 +35,8 @@ class Stats:
         for emoji_id in found_emojis:
             for emoji in ctx.message.guild.emojis:
                 if emoji_id == str(emoji):
-                    print(emoji.name)
-                    
+                    emoji_count[emoji] += 1
+        temp_str = 'Emoji use over last 30 days:'
+        for key, value in emoji_count:
+            temp_str += f'{key}: {value}\n'
+        ctx.send(temp_str)
