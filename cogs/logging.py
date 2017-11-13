db38e0a9d1bf7ab7ec3b7f0fab59aaaa758e04b8
dashwav <dashwav@gmail.com>
2017-11-13 11:39:35 -0700

    fix all the mistakes i just made

--- cogs/logging.py
+++ cogs/logging.py
@@ -11,23 +11,26 @@ class Logging():
         super().__init__()
 
     async def on_message(self, message):
-        if not message.channel isinstance(discord.DMChannel):
+        if not isinstance(message.channel, discord.DMChannel):
             return
         if message.author.bot:
             return
-        guild = self.get_guild(self.bot.guild_id)
+        guild = self.bot.get_guild(self.bot.guild_id)
         a_irl_member = guild.get_member(message.author.id)
         found_role = False
-        for role in member.roles:
-            if role.name.lower() = 'member':
+        for role in a_irl_member.roles:
+            if role.name.lower() == 'member':
                 found_role = True
         if found_role:
-            mod_info = self.get_channel(self.mod_info)
-            local_embed = discord.Embed(
-                title=f'DM report:',
-                description=message.clean_content
-            )
-            await mod_info.send(embed=local_embed)
+            try:
+                mod_info = self.bot.get_channel(self.bot.mod_info)
+                local_embed = discord.Embed(
+                    title=f'DM report:',
+                    description=message.clean_content
+                )
+                await mod_info.send(embed=local_embed)
+            except Exception as e:
+                self.bot.logger.warning(f'Issue forwarding dm: {e}')
 
     async def on_member_update(self, before, after):
         if before.roles != after.roles:
