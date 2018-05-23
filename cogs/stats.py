e50574871f2a7a9231a34de9fd92b73ef8db4d93
dashwav <dashwav@gmail.com>
2018-05-23 00:58:53 -0700

    actually shorten desc

--- cogs/stats.py
+++ cogs/stats.py
@@ -198,9 +198,11 @@ class Stats:
         for key in sorted(count_dict, key=count_dict.get, reverse=True):
             desc += f'{key}: {count_dict[key]}\n'
         if len(desc) > 2048:
+            desc = ''
             desc_over = ''
             count = 0
             temp = desc.split('\n')
+            desc = ''
             for emoji in temp:
                 count += len(emoji) + 1
                 self.bot.logger.info(count)
