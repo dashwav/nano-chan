7a132d78d92be10197695026c264a960f05a68b0
niealeh <niealeh+git@gmail.com>
2019-01-12 16:15:06 -0700

    Added username to footer of clover embed
    Turns out mobile hates embed mentions so I added plaintext names to the footers of the clover/member join embed probably.
    
    Please check this one too I probably forgot something again thank you for your patience.

--- cogs/logging.py
+++ cogs/logging.py
@@ -77,21 +77,23 @@ class Logging():
                 if role.name.lower() == 'clover':
                     await self.bot.postgres_controller.add_new_clover(after)
                     await mod_info.send(embed=discord.Embed(
-                            color=0x419400,
-                            title='Clover',
-                            description=f'**{time}: **Successfully applied clover to '
-                                        f'{after.mention}. [Joined: {join}]'))
+                            color = 0x419400,
+                            title = 'Clover',
+                            description = f'**{time}: **Successfully applied clover to '
+                                        f'{after.mention}. [Joined: {join}]',
+                            set_footer = f'{after}'))
                 elif role.name.lower() == 'member':
                     for role in before.roles:
                         if '🔑' in role.name.lower():
                             return
                         elif role.name.lower() == 'clover':
                             return
-                    await mod_info.send(embed=discord.Embed(
-                            color=0x3498DB,
-                            title='Member',
-                            description=f'**{time} **Successfully applied member to '
-                                        f'{after.mention}. [Joined: {join}]'))
+                    await mod_info.send(embed = discord.Embed(
+                            color = 0x3498DB,
+                            title = 'Member',
+                            description = f'**{time} **Successfully applied member to '
+                                        f'{after.mention}. [Joined: {join}]',
+                            set_footer = f'{after}'))
 
 def setup(bot):
     bot.add_cog(Logging(bot))
